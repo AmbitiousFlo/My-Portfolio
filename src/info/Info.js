9cb0ccecf799fa3eaf3c1cf6b4ce90e81e0261e9
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-25 18:48:57 -0500

    update info

--- src/info/Info.js
+++ src/info/Info.js
@@ -25,7 +25,7 @@ of what each of the values mean.
 export const info = {
     firstName: "Christian",
     lastName: "Flores",
-    initials: "Christian", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "CF", // the example uses first and last, but feel free to use three or more if you like.
     position: "A Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
