4fd54e3f8d8086511be6e7f488154db1394ba0f1
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-25 18:27:00 -0500

    updated images and sizing

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -8,10 +8,10 @@ function PortfolioBlock(props) {
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box component={'img'} src={image} alt={'mockup'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
-         <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
+         <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'1rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
+               <IconLink link={live} title={'Webpage'} icon={'fa fa-chrome'}/>
             </Box>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
                <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
