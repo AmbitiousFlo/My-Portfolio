ba3fd2aa19fb486b9a2f93d6a6de75eae31a0caa
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-10-25 17:22:31 -0500

    update to bio

--- src/info/Info.js
+++ src/info/Info.js
@@ -68,7 +68,7 @@ export const info = {
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello, I'm Christian Flores, a Full Stack Developer currently exploring the United States with my wife and our 1 year old son. I have had the honor of serving in the U.S. Air Force for 6 years, and I've also worked as an IT Support Specialist for the United States District Courts. Being a developer is not just rewarding in terms of creating innovative solutions, but it's also amazing how our code can impact the world, one line at a time.",
+    bio: "Hello, I'm Christian Flores, a Full Stack Developer currently exploring the United States with my wife and our 1 year old son. I have had the honor of serving in the U.S. Air Force for 6 years, and I've also worked as an IT Support Specialist for the United States District Courts for 3 years. Being a developer is not just rewarding in terms of creating innovative solutions, but it's also amazing how our code can impact the world, one line at a time.",
     skills:
         {
             proficientWith: ['Javascript', 'React', 'Git', 'Github', 'Bootstrap', 'HTML5', 'CSS3', 'NODEJS', 'JQUERY','MongoDB','Express'],
